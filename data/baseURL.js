2a976b56aa44facba0daf314b5fedbae60172ed8
Phil McCloghry-Laing <phil@philistine.com.au>
2022-08-19 15:23:29 +1000

    Add support for staging

--- data/baseURL.js
+++ data/baseURL.js
@@ -1,6 +1,18 @@
-const baseUrl ={
-    baseUrlLink: 'https://dev.asset.accountant',
+const baseUrl = {
+    get baseUrlLink() {
+        if (process.env.PLATFORM === 'prod') {
+            if (process.env.STAGING === 'ui') {
+                return 'https://app-aa-ui-aueast-prod-staging.azurewebsites.net/';
+            }
+            return 'https://app.asset.accountant';
+        } else {
+            if (process.env.STAGING === 'ui') {
+                return 'https://app-aa-ui-aueast-dev-staging.azurewebsites.net/';
+            }
+            return 'https://dev.asset.accountant';
+        }
+    },
     baseUrlIntuitLink: 'https://developer.intuit.com/',
-    baseUrlXeroLink: 'https://developer.xero.com/'
-}
+    baseUrlXeroLink: 'https://developer.xero.com/',
+};
 module.exports = baseUrl;
